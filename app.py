224b2d2f0188a35c67c2df60cbb0b87ff113927f
Axiom Coder <prettylioness0550@gmail.com>
2026-07-09 16:09:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from pyrogram.enums import ParseMode
 
 print("Starting bot...")
 
-API_ID = int(os.getenv("API_ID"))
+API_ID = int(os.getenv(API_ID))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URL = os.getenv("MONGO_URL")
